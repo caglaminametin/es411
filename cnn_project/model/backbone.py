3c2c5aa51976585b2fc8e088431640f8c85e4630
caglaminametin <105871539+caglaminametin@users.noreply.github.com>
2025-06-20 12:02:07 +0300

    Update backbone.py

--- cnn_project/model/backbone.py
+++ cnn_project/model/backbone.py
@@ -1,1 +1,37 @@
-<actual backbone.py code>
+from tensorflow.keras import layers, models
+
+def build_backbone(input_shape):
+    inputs = layers.Input(shape=input_shape)
+
+    # Block 1
+    x = layers.Conv2D(64, (3,3), padding='same', activation='linear')(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(64, (3,3), padding='same', activation='linear')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.MaxPooling2D((2,2))(x)
+
+    # Block 2
+    x = layers.Conv2D(128, (3,3), padding='same', activation='linear')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(128, (3,3), padding='same', activation='linear')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.MaxPooling2D((2,2))(x)
+
+    # Block 3
+    x = layers.Conv2D(256, (3,3), padding='same', activation='linear')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(256, (3,3), padding='same', activation='linear')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(256, (3,3), padding='same', activation='linear')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.MaxPooling2D((2,2))(x)
+
+    model = models.Model(inputs, x, name="CustomBackbone")
+    return model
