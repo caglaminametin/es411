ccb529bbe6cf9ecd0f3b036421f12a9001bedb55
caglaminametin <105871539+caglaminametin@users.noreply.github.com>
2025-06-20 12:05:33 +0300

    Update plotting.py

--- cnn_project/utils/plotting.py
+++ cnn_project/utils/plotting.py
@@ -1,1 +1,18 @@
-<actual plotting.py code>
+import matplotlib.pyplot as plt
+
+def plot_training(history):
+    plt.figure(figsize=(14,5))
+
+    plt.subplot(1,2,1)
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Val Loss')
+    plt.title('Loss')
+    plt.legend()
+
+    plt.subplot(1,2,2)
+    plt.plot(history.history['accuracy'], label='Train Acc')
+    plt.plot(history.history['val_accuracy'], label='Val Acc')
+    plt.title('Accuracy')
+    plt.legend()
+
+    plt.show()
