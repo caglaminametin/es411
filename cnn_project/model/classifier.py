367d7fe7cb161e420a997272c44d1e7172130e56
caglaminametin <105871539+caglaminametin@users.noreply.github.com>
2025-06-20 12:02:24 +0300

    Update classifier.py

--- cnn_project/model/classifier.py
+++ cnn_project/model/classifier.py
@@ -1,1 +1,11 @@
-<actual classifier.py code>
+from tensorflow.keras import layers, models
+from .backbone import build_backbone
+
+def create_cnn_model(input_shape, num_classes):
+    backbone = build_backbone(input_shape)
+    x = layers.GlobalAveragePooling2D()(backbone.output)
+    x = layers.Dense(512, activation='relu')(x)
+    x = layers.Dropout(0.6)(x)
+    outputs = layers.Dense(num_classes, activation='softmax')(x)
+    model = models.Model(inputs=backbone.input, outputs=outputs)
+    return model
