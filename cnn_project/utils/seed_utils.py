60ddf0912f26f0f04d97302ec10cac27069655c0
caglaminametin <105871539+caglaminametin@users.noreply.github.com>
2025-06-20 12:01:43 +0300

    Update seed_utils.py

--- cnn_project/utils/seed_utils.py
+++ cnn_project/utils/seed_utils.py
@@ -1,1 +1,8 @@
-<actual seed_utils.py code>
+import random
+import numpy as np
+import tensorflow as tf
+
+def set_seeds(seed=42):
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
